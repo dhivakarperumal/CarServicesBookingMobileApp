c49bd8c5f96de0e96c82d8dfe05f4c3a55d75978
Dhivakar <dhivakarp305@gmail.com>
2026-02-23 11:38:44 +0530

    fix it role

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -52,7 +52,7 @@ export default function LoginScreen() {
       const role = userSnap.data().role?.toLowerCase();
 
       if (role === "admin") router.replace("/(adminTabs)/home");
-      else if (role === "role") router.replace("/(EmployeesDash)/dashboard");
+      else if (role === "Mechanic") router.replace("/(EmployeesDash)/dashboard");
       else router.replace("/(tabs)/home");
     } catch (error) {
       Alert.alert("Login Failed", error.message);
